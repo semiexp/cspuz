53ff1d697ab747dc5c9096566507ab8d819cb258
semiexp <linthya.v@gmail.com>
2020-02-09 17:51:45 +0900

    Use two bool array (horizontal, vertical) to store edges in BoolGridFrame

--- cspuz/grid_frame.py
+++ cspuz/grid_frame.py
@@ -1,3 +1,5 @@
+import itertools
+
 from cspuz.constraints import BoolVars
 from cspuz.grid import BoolGrid
 
@@ -10,25 +12,22 @@ class BoolGridFrame(object):
         self.solver = solver
         self.height = height
         self.width = width
-        self.edges = []
-        self._all_edges = []
-        for y in range(2 * height + 1):
-            row = []
-            for x in range(2 * width + 1):
-                if y % 2 != x % 2:
-                    v = solver.bool_var()
-                    row.append(v)
-                    self._all_edges.append(v)
-                else:
-                    row.append(None)
-            self.edges.append(row)
+        self.horizontal = solver.bool_array((height + 1, width))
+        self.vertical = solver.bool_array((height, width + 1))
 
     def __getitem__(self, item):
         y, x = item
-        return self.edges[y][x]
+        if not (0 <= y <= self.height * 2 and 0 <= x <= self.width * 2):
+            raise IndexError('index out of range')
+        if y % 2 == 0 and x % 2 == 1:
+            return self.horizontal[y // 2, x // 2]
+        elif y % 2 == 1 and x % 2 == 0:
+            return self.vertical[y // 2, x // 2]
+        else:
+            raise IndexError('index does not specify a loop edge')
 
     def all_edges(self):
-        return BoolVars(self._all_edges)
+        return BoolVars(list(itertools.chain(self.horizontal, self.vertical)))
 
     def single_loop(self):
         solver = self.solver
