af4ff5fbd0b75c91f82cc58cf1fc31f55b57be69
semiexp <linthya.v@gmail.com>
2020-04-05 19:59:23 +0900

    Support checkered fillomino

--- cspuz/puzzle/fillomino.py
+++ cspuz/puzzle/fillomino.py
@@ -5,7 +5,7 @@ from cspuz.puzzle import util
 from cspuz.generator import generate_problem, count_non_default_values, ArrayBuilder2D
 
 
-def solve_fillomino(height, width, problem):
+def solve_fillomino(height, width, problem, checkered=False):
     solver = Solver()
     size = solver.int_array((height, width), 1, height * width)
     solver.add_answer_key(size)
@@ -16,15 +16,19 @@ def solve_fillomino(height, width, problem):
         for x in range(width):
             if problem[y][x] >= 1:
                 solver.ensure(size[y, x] == problem[y][x])
+    if checkered:
+        color = solver.bool_array((height, width))
+        solver.ensure((group_id[:, :-1] == group_id[:, 1:]) == (color[:, :-1] == color[:, 1:]))
+        solver.ensure((group_id[:-1, :] == group_id[1:, :]) == (color[:-1, :] == color[1:, :]))
     is_sat = solver.solve()
     return is_sat, size
 
 
-def generate_fillomino(height, width, disallow_adjacent=False, symmetry=False, verbose=False):
-    generated = generate_problem(lambda problem: solve_fillomino(height, width, problem),
+def generate_fillomino(height, width, checkered=False, disallow_adjacent=False, symmetry=False, verbose=False):
+    generated = generate_problem(lambda problem: solve_fillomino(height, width, problem, checkered=checkered),
                                  builder_pattern=ArrayBuilder2D(height, width, range(0, 9), default=0,
                                                                 disallow_adjacent=disallow_adjacent, symmetry=symmetry),
-                                 clue_penalty=lambda problem: count_non_default_values(problem, default=0, weight=3),
+                                 clue_penalty=lambda problem: count_non_default_values(problem, default=0, weight=5),
                                  verbose=verbose)
     return generated
 
@@ -52,7 +56,7 @@ def _main():
     else:
         height, width = map(int, sys.argv[1:])
         while True:
-            problem = generate_fillomino(height, width, verbose=True)
+            problem = generate_fillomino(height, width, checkered=True, symmetry=True, verbose=True)
             if problem is not None:
                 print(util.stringify_array(problem, lambda x: '.' if x == 0 else str(x)), flush=True)
                 print(flush=True)
