7790d5350cf4a15f8156dde3da7be65dcc704ba7
semiexp <linthya.v@gmail.com>
2020-04-29 23:17:35 +0900

    Add a test for serializing star battle problems

---
+++ tests/puzzle/test_star_battle.py
@@ -0,0 +1,16 @@
+from cspuz.puzzle import star_battle
+
+
+def test_serialization():
+    n = 6
+    k = 1
+    block_id = [
+        [0, 0, 0, 0, 1, 1],
+        [0, 2, 3, 0, 1, 1],
+        [2, 2, 3, 3, 3, 1],
+        [2, 1, 1, 1, 1, 1],
+        [2, 4, 4, 1, 4, 5],
+        [2, 2, 4, 4, 4, 5],
+    ]
+    expected = 'http://pzv.jp/p.html?starbattle/6/6/1/2u9gn9c9jpmk'
+    assert star_battle.problem_to_pzv_url(n, k, block_id) == expected
