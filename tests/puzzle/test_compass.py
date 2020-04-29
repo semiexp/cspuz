2c152e3394d9d98d29a2e3ea56d1a9e90f682cde
semiexp <linthya.v@gmail.com>
2020-04-29 23:33:07 +0900

    Change the test for serialization of Compass

--- tests/puzzle/test_compass.py
+++ tests/puzzle/test_compass.py
@@ -3,12 +3,11 @@ from cspuz.puzzle import compass
 
 def test_serialization():
     height = 5
-    width = 5
+    width = 4
     problem = [
-        (1, 2, -1, 1, -1, -1),
-        (2, 1, 2, -1, 5, 1),
-        (2, 3, 5, -1, 3, -1),
-        (3, 2, 1, -1, -1, 1)
+        (1, 1, 1, 2, -1, 3),
+        (2, 3, -1, 6, -1, -1),
+        (3, 1, 4, -1, -1, 5)
     ]
-    expected = 'https://puzz.link/p?compass/5/5/m..1.i25.1g53..i1..1m'
+    expected = 'https://puzz.link/p?compass/4/5/k1.23k..6.g4..5l'
     assert compass.to_puzz_link_url(height, width, problem) == expected
