42b1ac0ab8576c4f516df3f9d2c1423354c37a06
semiexp <linthya.v@gmail.com>
2020-04-29 23:08:03 +0900

    Add functions for serializing arrays as pzpr URL

--- cspuz/puzzle/util.py
+++ cspuz/puzzle/util.py
@@ -48,3 +48,101 @@ def stringify_grid_frame(grid_frame):
                 res.append(' ')
         res.append('\n')
     return ''.join(res)
+
+
+_BASE36 = '0123456789abcdefghijklmnopqrstuvwxyz'
+
+
+def _encode_int_or_str(v):
+    if isinstance(v, str):
+        return v
+    else:
+        if v <= 15:
+            prefix = ''
+        elif v <= 255:
+            prefix = '-'
+        elif v <= 4095:
+            prefix = '+'
+        else:
+            raise ValueError('too large value: {}'.format(v))
+        return prefix + hex(v)[2:]
+
+
+def map2d(func, it):
+    return list(map(lambda x: list(map(func, x)), it))
+
+
+def encode_array(array, single_empty_marker='g', empty=None, dim=None):
+    """
+    Encode a 1-D or 2-D array into a serialized string in the pzpr format.
+    :param array: a 1-D or 2-D array to be serialized.
+    Each element of `array` should be one of:
+    - `empty` representing the empty cell,
+    - a str to be serialized as-is,
+    - an int to be serialized in base-16, or
+    - a `list` of `str`s or `int`s.
+    :param single_empty_marker: the number (in base-36) representing single empty cell in the serialization.
+    :param empty: the value to represent empty cells.
+    :param dim: the number of dimensions (1 or 2) of `array`. If `None` is specified, it is automatically inferred.
+    :return: a str representing the serialization of `array`.
+    """
+    single_empty_index = _BASE36.index(single_empty_marker)
+
+    if dim is None:
+        is_two_dim = True
+        for v in array:
+            if not isinstance(v, list):
+                is_two_dim = False
+        if is_two_dim:
+            dim = 2
+        else:
+            dim = 1
+    else:
+        if dim not in (1, 2):
+            raise ValueError('invalid value of dim')
+    if dim == 2:
+        # flatten
+        array = sum(array, [])
+    res = []
+    contiguous_empty_cells = 0
+    for v in array:
+        if v == empty:
+            contiguous_empty_cells += 1
+            if contiguous_empty_cells - 1 + single_empty_index >= 36:
+                res.append(_BASE36[-1])
+                contiguous_empty_cells = 1
+        else:
+            if contiguous_empty_cells > 0:
+                res.append(_BASE36[contiguous_empty_cells - 1 + single_empty_index])
+                contiguous_empty_cells = 0
+            if isinstance(v, (str, int)):
+                res.append(_encode_int_or_str(v))
+            elif isinstance(v, list):
+                for w in v:
+                    res.append(_encode_int_or_str(w))
+            else:
+                raise TypeError('unsupported type for serialization')
+    return ''.join(res)
+
+
+def encode_grid_segmentation(height, width, block_id):
+    def convert_binary_seq(s):
+        ret = ''
+        for i in range((len(s) + 4) // 5):
+            v = 0
+            for j in range(5):
+                if i * 5 + j < len(s) and s[i * 5 + j] == 1:
+                    v += (2 ** (4 - j))
+            ret += _BASE36[v]
+        return ret
+    s = []
+    for y in range(height):
+        for x in range(width - 1):
+            s.append(1 if block_id[y][x] != block_id[y][x + 1] else 0)
+    ret = convert_binary_seq(s)
+    s = []
+    for y in range(height - 1):
+        for x in range(width):
+            s.append(1 if block_id[y][x] != block_id[y + 1][x] else 0)
+    ret += convert_binary_seq(s)
+    return ret
