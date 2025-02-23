901b40c1947674de0ba6fb3190f3466921eff3d0
semiexp <linthya.v@gmail.com>
2025-02-23 10:34:07 +0900

    Add typing to test_backend.py

--- tests/test_backend.py
+++ tests/test_backend.py
@@ -3,33 +3,35 @@ import pytest
 import cspuz
 
 
-class TestBackend:
-    # TODO: test sugar, sugar_extended, csugar and enigma_csp
-    @pytest.fixture(autouse=True, params=["z3", "cspuz_core"])
-    def default_backend(self, request):
-        cspuz.config.default_backend = request.param
+# TODO: test sugar, sugar_extended, csugar and enigma_csp
+@pytest.fixture(autouse=True, params=["z3", "cspuz_core"])
+def default_backend(request: pytest.FixtureRequest) -> None:
+    cspuz.config.default_backend = request.param
 
-    @pytest.fixture
-    def solver(self):
-        return cspuz.Solver()
 
-    def test_find_answer(self, solver):
-        x = solver.bool_var()
-        y = solver.bool_var()
+@pytest.fixture
+def solver() -> cspuz.Solver:
+    return cspuz.Solver()
 
-        solver.ensure(x.then(y))
-        solver.ensure((~x).then(y))
 
-        assert solver.find_answer()
+def test_find_answer(solver: cspuz.Solver) -> None:
+    x = solver.bool_var()
+    y = solver.bool_var()
 
-    def test_solve(self, solver):
-        x = solver.bool_var()
-        y = solver.bool_var()
+    solver.ensure(x.then(y))
+    solver.ensure((~x).then(y))
 
-        solver.ensure(x.then(y))
-        solver.ensure((~x).then(y))
-        solver.add_answer_key(x, y)
+    assert solver.find_answer()
 
-        assert solver.solve()
-        assert x.sol is None
-        assert y.sol is True
+
+def test_solve(solver: cspuz.Solver) -> None:
+    x = solver.bool_var()
+    y = solver.bool_var()
+
+    solver.ensure(x.then(y))
+    solver.ensure((~x).then(y))
+    solver.add_answer_key(x, y)
+
+    assert solver.solve()
+    assert x.sol is None
+    assert y.sol is True
